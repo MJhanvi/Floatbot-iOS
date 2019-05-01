abcf5471c0423e9a71b1abc01ee843e9a16ea1bf
MJhanvi <33341002+MJhanvi@users.noreply.github.com>
2019-05-01 19:42:53 +0530

    Add files via upload

---
+++ Demo.framework/Headers/Demo.h
@@ -0,0 +1,19 @@
+//
+//  Demo.h
+//  Demo
+//
+//  Created by MEHUL PANCHAL on 01/05/19.
+//  Copyright © 2019 MEHUL PANCHAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Demo.
+FOUNDATION_EXPORT double DemoVersionNumber;
+
+//! Project version string for Demo.
+FOUNDATION_EXPORT const unsigned char DemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Demo/PublicHeader.h>
+
+
